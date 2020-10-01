2e89161a5a77da99500f37a34a655afd7a877a35
Viacheslav Zholnovach <vyajol@gmail.com>
2020-10-01 10:06:41 +0300

    Oct 01

---
+++ c#/Problems/2020/Oct/Oct01.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Challenge
+{
+  /// <summary>
+  ///		https://leetcode.com/explore/featured/card/october-leetcoding-challenge/559/week-1-october-1st-october-7th/3480/
+  ///		https://leetcode.com/submissions/detail/403015981/?from=/explore/featured/card/october-leetcoding-challenge/559/week-1-october-1st-october-7th/3480/
+  ///	</summary>
+  internal class Oct01
+  {
+    public class RecentCounter
+    {
+      LinkedList<int> exp = new LinkedList<int>();
+
+      public RecentCounter()
+      {
+      }
+
+      public int Ping(int t)
+      {
+        return Add(t);
+      }
+
+      int Add(int value)
+      {
+        var expirationValue = value + 3000;
+
+        if (exp.First == null || expirationValue <= exp.First.Value)
+        {
+          exp.AddFirst(expirationValue);
+          return 1;
+        }
+
+        var current = exp.First;
+        while (current != null)
+        {
+          if (expirationValue == current.Value)
+          {
+            exp.AddBefore(current, expirationValue);
+            return exp.Count;
+          }
+
+          if (expirationValue < current.Value)
+          {
+            exp.AddAfter(current.Previous, expirationValue);
+            return exp.Count;
+          }
+
+          var tmpCurrent = current;
+          current = current.Next;
+
+          if (tmpCurrent.Value < value)
+            exp.RemoveFirst();
+        }
+
+        if (exp.Last == null)
+        {
+          exp.AddFirst(expirationValue);
+        }
+        else
+        {
+          exp.AddAfter(exp.Last, expirationValue);
+        }
+
+        return exp.Count;
+      }
+    }
+
+    /**
+     * Your RecentCounter object will be instantiated and called as such:
+     * RecentCounter obj = new RecentCounter();
+     * int param_1 = obj.Ping(t);
+     */
+  }
+}
