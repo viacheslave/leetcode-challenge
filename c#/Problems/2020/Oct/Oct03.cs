5f2d95428c294a27b6d7e923b23efd79ff3f07d3
Viacheslav Zholnovach <vyajol@gmail.com>
2020-10-04 10:29:33 +0300

    Oct 03-04

---
+++ c#/Problems/2020/Oct/Oct03.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Challenge
+{
+  /// <summary>
+  ///		https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/559/week-1-october-1st-october-7th/3482/
+  ///		
+  ///	</summary>
+  internal class Oct03
+  {
+    public class Solution
+    {
+      public int FindPairs(int[] nums, int k)
+      {
+        if (nums.Length <= 1)
+          return 0;
+
+        Array.Sort(nums);
+
+        var hs = new HashSet<(int, int)>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+          for (var j = i + 1; j < nums.Length; j++)
+          {
+            if (nums[j] - nums[i] < k)
+              continue;
+
+            if (nums[j] - nums[i] == k)
+            {
+              hs.Add((nums[i], nums[j]));
+              continue;
+            }
+
+            if (nums[j] - nums[i] > k)
+              break;
+          }
+        }
+
+        return hs.Count;
+      }
+    }
+  }
+}
---
+++ c#/Problems/2020/Oct/Oct04.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Challenge
+{
+  /// <summary>
+  ///		https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/559/week-1-october-1st-october-7th/3483/
+  ///		https://leetcode.com/submissions/detail/404290702/?from=/explore/challenge/card/october-leetcoding-challenge/559/week-1-october-1st-october-7th/3483/
+  ///	</summary>
+  internal class Oct04
+  {
+    public class Solution
+    {
+      public int RemoveCoveredIntervals(int[][] intervals)
+      {
+        var ans = new List<int>();
+
+        for (var i = 0; i < intervals.Length; i++)
+        {
+          for (var j = 0; j < intervals.Length; j++)
+          {
+            if (i == j)
+              continue;
+
+            if (Covered(intervals, i, j))
+            {
+              ans.Add(i);
+              break;
+            }
+          }
+        }
+
+        return intervals.Length - ans.Count;
+      }
+
+      private bool Covered(int[][] intervals, int i, int j)
+      {
+        var th = intervals[i];
+        var other = intervals[j];
+
+        return other[0] <= th[0] && th[1] <= other[1];
+      }
+    }
+  }
+}
