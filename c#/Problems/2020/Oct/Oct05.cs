8b98ad25cda77ef6d5788c24c60aa59f1db0b638
Viacheslav Zholnovach <vyajol@gmail.com>
2020-10-06 10:59:35 +0300

    Oct 05-06

---
+++ c#/Problems/2020/Oct/Oct05.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Challenge
+{
+  /// <summary>
+  ///		https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/559/week-1-october-1st-october-7th/3484/
+  ///
+  ///	</summary>
+  internal class Oct05
+  {
+    public class Solution
+    {
+      public int BitwiseComplement(int N)
+      {
+        if (N == 0)
+          return 1;
+
+        var original = N;
+        var power = 0;
+        var comp = 0;
+
+        while (true)
+        {
+          if (power == 31)
+            break;
+
+          if (original >= (int)Math.Pow(2, power))
+          {
+            var digit = N % 2;
+
+            if (digit == 0)
+              comp += (int)Math.Pow(2, power);
+
+            N = N >> 1;
+            power++;
+            continue;
+          }
+          break;
+        }
+
+        return comp;
+      }
+    }
+  }
+}
