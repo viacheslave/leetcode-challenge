1364cd2d0fb022565a880f257fd1b67af9733d75
Viacheslav Zholnovach <vyajol@gmail.com>
2020-11-08 10:06:29 +0200

    Nov 07, Nov 08

---
+++ c#/Problems/2020/11/Nov07.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Challenge
+{
+  /// <summary>
+  ///    https://leetcode.com/explore/challenge/card/november-leetcoding-challenge/564/week-1-november-1st-november-7th/3522/
+  ///    
+  /// </summary>
+  internal class Nov07
+  {
+    /**
+     * Definition for singly-linked list.*/
+    public class ListNode {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+    
+    public class Solution
+    {
+      public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+      {
+        var s1 = GetStack(l1);
+        var s2 = GetStack(l2);
+
+        ListNode current = null;
+
+        var carry = 0;
+        while (s1.Count > 0 || s2.Count > 0)
+        {
+          var val1 = s1.Count > 0 ? s1.Pop() : 0;
+          var val2 = s2.Count > 0 ? s2.Pop() : 0;
+
+          var node = new ListNode(val1 + val2 + carry);
+          if (node.val >= 10)
+          {
+            node.val = node.val - 10;
+            carry = 1;
+          }
+          else
+          {
+            carry = 0;
+          }
+
+          node.next = current;
+          current = node;
+        }
+
+        if (carry == 1)
+        {
+          var node = new ListNode(carry);
+          node.next = current;
+          current = node;
+        }
+
+        return current;
+      }
+
+      Stack<int> GetStack(ListNode node)
+      {
+        var s = new Stack<int>();
+
+        var current = node;
+        if (current == null)
+          return s;
+
+        s.Push(current.val);
+
+        while (current.next != null)
+        {
+          s.Push(current.next.val);
+          current = current.next;
+        }
+
+        return s;
+      }
+    }
+  }
+}
---
+++ c#/Problems/2020/11/Nov08.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Challenge
+{
+  /// <summary>
+  ///    https://leetcode.com/explore/challenge/card/november-leetcoding-challenge/565/week-2-november-8th-november-14th/3524/
+  ///    
+  /// </summary>
+  internal class Nov08
+  {
+    /**
+     * Definition for a binary tree node.*/
+    public class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+    
+    public class Solution
+    {
+      public int full = 0;
+
+      public int FindTilt(TreeNode root)
+      {
+        int sum = GetSum(root);
+        return full;
+      }
+
+      private int GetSum(TreeNode node)
+      {
+        if (node == null)
+          return 0;
+
+        var left = GetSum(node.left);
+        var right = GetSum(node.right);
+
+        full += Math.Abs(left - right);
+
+        return left + right + node.val;
+      }
+    }
+  }
+}
