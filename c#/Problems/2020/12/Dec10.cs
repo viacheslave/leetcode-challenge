417cf71bcb85329dc46afdc77f9c2e9709e5711e
Viacheslav Zholnovach <vyajol@gmail.com>
2020-12-10 10:16:38 +0200

    Dec 10

---
+++ c#/Problems/2020/12/Dec10.cs
@@ -0,0 +1,53 @@
+﻿namespace LeetCode.Challenge
+{
+  /// <summary>
+  ///    https://leetcode.com/explore/challenge/card/december-leetcoding-challenge/570/week-2-december-8th-december-14th/3561/
+  ///    https://leetcode.com/submissions/detail/429161253/?from=explore&item_id=3561
+  /// </summary>
+  internal class Dec10
+  {
+    public class Solution
+    {
+      public bool ValidMountainArray(int[] A)
+      {
+        if (A.Length < 3)
+          return false;
+
+        var up = true;
+
+        for (var i = 1; i < A.Length; i++)
+        {
+          if (A[i] == A[i - 1])
+            return false;
+
+          if (up)
+          {
+            if (A[i] > A[i - 1])
+              continue;
+
+            if (i == 1)
+              return false;
+
+            else
+              up = false;
+
+            continue;
+          }
+          else
+          {
+            if (A[i] < A[i - 1])
+              continue;
+            else
+              return false;
+          }
+
+        }
+
+        if (up)
+          return false;
+
+        return true;
+      }
+    }
+  }
+}
